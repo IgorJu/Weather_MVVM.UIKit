7fb7423bce9be0f2106947bbbf59c9739624d6e0
Igor Rukaleev <575714470@mail.ru>
2023-10-21 15:51:46 +0300

    Initial Commit

---
+++ Weather_MVVM.UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather_MVVM.UIKit
+//
+//  Created by Igor on 21.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
